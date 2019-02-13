aaa4eb4a15ca7a0b580c15b0183e1e2cde58b1f7
Axolotl <samuelmachat@gmail.com>
2019-02-13 16:15:26 -0500

    add classes for players and rooms

--- src/room.py
+++ src/room.py
@@ -1,2 +1,8 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+
+class Room:
+    def __init__(self, name, text):
+        self.name = name
+        self.text = text
